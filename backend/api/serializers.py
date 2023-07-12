9558799339ef7111dec4b04eb26c20ccb2e7370a
Arustamian-Artem <116645409+Arustamian-Artem@users.noreply.github.com>
2023-07-12 20:26:23 +0300

    ver 2.1

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,8 +5,14 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 
 from api.fields import Base64ImageField
-from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
-                            ShoppingCart, Tag)
+from recipes.models import (
+    Favorite,
+    Ingredient,
+    Recipe,
+    RecipeIngredient,
+    ShoppingCart,
+    Tag
+)
 from users.models import Subscribe, User
 
 
